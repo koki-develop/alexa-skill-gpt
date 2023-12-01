02468c4e1a345410c2f6354a54dc9b14dacab38f
koki-develop <kou.pg.0131@gmail.com>
2023-12-02 08:54:40 +0900

    Remove HelloWorldIntentHandler

--- lambda/index.js
+++ lambda/index.js
@@ -63,21 +63,6 @@ const GPTIntentHandler = {
   },
 };
 
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-    const speakOutput = 'Hello World!';
-
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-      .getResponse();
-  }
-};
-
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -189,7 +174,6 @@ exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
     GPTIntentHandler,
-    HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     FallbackIntentHandler,
